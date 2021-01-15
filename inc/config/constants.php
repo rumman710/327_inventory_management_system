3dbfedebfe1f3574bd2cc539ad573a43de8a8729
rumman710 <71409143+rumman710@users.noreply.github.com>
2021-01-15 22:54:26 +0600

    Add files via upload

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://localhost/inventory/');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost;dbname=inventory_management');
+	
+	// Hostname
+	define('DB_HOST', 'localhost');
+	
+	// DB user
+	define('DB_USER', 'inventoryUser');
+	
+	// DB password
+	define('DB_PASSWORD', 'password');
+	
+	// DB name
+	define('DB_NAME', 'inventory_management');
+?>
